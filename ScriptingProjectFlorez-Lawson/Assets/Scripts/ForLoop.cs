9e26753fd31fee0f105a288113b8014563924f6a
Matthewmfl <735518@student.dusd.net>
2022-11-02 09:01:27 -0700

    Completed Loops
    Added DoWhileLoop and Foreachloop

--- ScriptingProjectFlorez-Lawson/Assets/Scripts/ForLoop.cs
+++ ScriptingProjectFlorez-Lawson/Assets/Scripts/ForLoop.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine; 
 
-public class Loops : MonoBehaviour
+public class ForLoop : MonoBehaviour
 {
     int numEnemies = 3;
     // Start is called before the first frame update

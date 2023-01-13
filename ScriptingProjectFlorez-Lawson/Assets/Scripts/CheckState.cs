7e2453cf8a6bab27e2db16d16e9fbbc56708d5e9
Matthewmfl <735518@student.dusd.net>
2023-01-13 08:55:55 -0800

    Completed Activating Game Objects
    I finished the activating game object script.

---
+++ ScriptingProjectFlorez-Lawson/Assets/Scripts/CheckState.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckState : MonoBehaviour
+{
+    public GameObject myObject;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Active Self: " + myObject.activeSelf);
+        Debug.Log("Active in Hierachy" + myObject.activeInHierarchy);
+    }
+}
+
+   
